00bba0643ace4086175fda5e83e9d436bc69a554
Howard <howardmy3@gmail.com>
2022-06-12 04:34:30 +0800

    first commit

---
+++ RandomSelector/main.js
@@ -0,0 +1,11 @@
+
+    $(function(){
+        $("input").on("click",function(){
+        //alert("Hi");
+        var numberOfListItem = $("li").length;
+        var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
+        $("h1").text($("li").eq(randomChildNumber).text());
+        document.getElementById('randing').src=randomChildNumber+'.jpg';
+        });
+        });
+        

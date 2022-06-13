42014fadaa0130af03a7aea3cfb73648406c9938
Howard <howardmy3@gmail.com>
2022-06-13 16:05:50 +0800

    first commit

---
+++ HelloVue/main.js
@@ -0,0 +1,34 @@
+let pageData = {
+    productName: "訂張票到月球",
+    productDescription: "月球之旅是搭乘我們最高級的飛行船，可以觀賞外太空的風景。",
+    imageSrc:[
+        "images/asteroid.jpg",
+        "images/fantasy.jpg",
+        "images/space.jpg",
+        "images/spaceship.jpg"
+    ],
+    h2ClassController:{
+        centered:true,
+        colorFont:false
+    },
+    pStyleController:{
+        'margin-left':'50px',
+        color:'blue',
+        'font-size':'20px',
+        'fomt-style':'italic'
+    },
+    imageStyleController:{
+        margin:'auto',
+        display:'block',
+        width:'50%'
+    },
+    imageAlt:"Photo from space"
+};
+
+const App = Vue.createApp({
+    data(){
+        return pageData;
+    }
+});
+
+App.mount("#app");
